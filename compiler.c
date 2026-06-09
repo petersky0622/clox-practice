f31fb757ef059c619b7297b31ceadf973fee9b94
lhb <peter346789@gmail.com>
2026-06-10 00:27:15 +0800

    finish scanner and start compiler

--- compiler.c
+++ compiler.c
@@ -1,28 +1,127 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <common.h>
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    
+typedef struct
+{
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+Parser parser;
+
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
+
+static void errorAt(Token *token, const char *message)
+{
+    if (parser.panicMode)
+        return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF)
+    {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type == TOKEN_ERROR)
+    {
+    }
+    else
+    {
+        fprintf(stderr, "at '%.*s'", token->length, token->start);
+    }
+
+    parser.hadError = true;
+}
+
+static void error(const char *message)
+{
+
+    errorAt(&parser.current, message);
+}
+
+static void errorAtCurrent(const char *message)
+{
+    errorAt(&parser.current, message);
+}
+
+static void advance()
+{
+    parser.previous = parser.current;
+
+    for (;;)
+    {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR)
+            break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void consume(TokenType type, const char *message)
+{
+    if (parser.current.type == type)
+    {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.current.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static void emitConstant(Value value) {
+    emitBytes(OP_CONSTANT, )
+}
+
+static void endcompiler() {
+    emitReturn();
+}
+
+static void number() {
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void expression() {
+
+}
+
+bool compile(const char *source, Chunk *chunk)
+{
+
     initScanner(source);
 
-    int line = -1;
-    for (;;) {
+    compilingChunk = chunk;
+    parser.hadError = false;
+    parser.panicMode = false;
 
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d", token.line);
-            line = token.line;
-        }
-        else {
-            printf("   | ");
-        }
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 
-        printf("%2d '%.*s'\n", token.type, token.length, token.start); // 印出token 資訊;
-        if (token.type == TOKEN_EOF) {
-            break;
-        }
+    endCompiler();
 
-    }
+    return !parser.hadError;
 }
--- vm.c
+++ vm.c
@@ -6,96 +6,131 @@
 
 VM vm;
 
-static void resetStack() {
+static void resetStack()
+{
     vm.stackTop = vm.stack;
 }
 
-void initVM() {
+void initVM()
+{
     resetStack();
 }
 
-void freeVM() {
-
+void freeVM()
+{
 }
 
-void push(Value value) {
+void push(Value value)
+{
     *vm.stackTop++ = value;
 }
 
-Value pop() {
+Value pop()
+{
     return *--vm.stackTop;
 }
 
-static Value readConstantLong() {
+static Value readConstantLong()
+{
     uint32_t constant = *vm.ip++;
     constant |= (uint32_t)(*vm.ip++) << 8;
     constant |= (uint32_t)(*vm.ip++) << 16;
     return vm.chunk->constants.values[constant];
 }
 
-static InterpretResult run() {
+static InterpretResult run()
+{
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_CONSTANT_LONG() readConstantLong()
 
-#define BINARY_OP(op) \
-    do { \
-      double b = pop(); \
-      double a = pop(); \
-      push(a op b); \
+#define BINARY_OP(op)     \
+    do                    \
+    {                     \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b);     \
     } while (false)
 
     uint8_t instruction;
-    for (;;) {
+    for (;;)
+    {
 
 #ifdef DEBUG_TRACE_EXECUTION
-    
-    printf("          ");
-    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
-        printf("[ ");
-        printValue(*slot);
-        printf(" ]");
-    }
-    printf("\n");
-    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+
+        printf("          ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 
 #endif
 
-        switch (instruction = READ_BYTE()){
-            case OP_CONSTANT:{
-                Value constant = READ_CONSTANT();
-                push(constant);
-                break;
-            }
-            case OP_CONSTANT_LONG:{
-                Value constant = READ_CONSTANT_LONG();
-                push(constant);
-                break;
-            }
-            case OP_ADD:       BINARY_OP(+); break;
-            case OP_SUBTRACT:  BINARY_OP(-); break;
-            case OP_MULTIPLY:  BINARY_OP(*); break;
-            case OP_DIVIDE:    BINARY_OP(/); break;
-            case OP_NEGATE:{
-                push(-pop());
-                break;
-            }
-            case OP_RETURN:{
-                printValue(pop());
-                printf("\n");
-                return INTERPRET_OK;
-            
-            }
+        switch (instruction = READ_BYTE())
+        {
+        case OP_CONSTANT:
+        {
+            Value constant = READ_CONSTANT();
+            push(constant);
+            break;
+        }
+        case OP_CONSTANT_LONG:
+        {
+            Value constant = READ_CONSTANT_LONG();
+            push(constant);
+            break;
+        }
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
+        case OP_NEGATE:
+        {
+            push(-pop());
+            break;
+        }
+        case OP_RETURN:
+        {
+            printValue(pop());
+            printf("\n");
+            return INTERPRET_OK;
+        }
         }
     }
-#undef  READ_BYTE
-#undef  READ_CONSTANT
-#undef  READ_CONSTANT_LONG
-#undef  BINARY_OP
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef READ_CONSTANT_LONG
+#undef BINARY_OP
 }
 
-InterpretResult interpret(const char* source) {
-    compile(source);
+InterpretResult interpret(const char *source)
+{
+    Chunk chunk;
+    initChunk(&chunk);
 
-    return INTERPRET_OK;
+    if (!compile(source, &chunk))
+    {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
