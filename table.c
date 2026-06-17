fb7bf85f3f7bd2804c9c029c6d2dbee060f459d4
lhb <peter346789@gmail.com>
2026-06-18 01:06:07 +0800

    hash table not finished again

--- table.c
+++ table.c
@@ -38,6 +38,16 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key)
     }
 }
 
+bool tableGet(Table* table, ObjString* key, Value* value) {
+    if (table->count == 0) return false;
+
+    Entry* entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL) return false;
+
+    *value = entry->value;
+    return true;
+}
+
 static adjustCapacity(Table *table, int capacity)
 {
     Entry *entries = ALLOCATE(Entry, capacity);
